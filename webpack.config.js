97b32fe3724cc260f3a5b3ace3ba7c28cc59589a
Vladislav <vlad.melnikov.post@gmail.com>
2017-11-11 21:02:34 +0300

    Init commit

---
+++ webpack.config.js
@@ -0,0 +1,39 @@
+var path = require("path");
+
+var DIST_DIR = path.resolve(__dirname, "dist");
+var SRC_DIR = path.resolve(__dirname, "src");
+
+var config = {
+	entry: SRC_DIR + "/app/index.js",
+	output: {
+		path: DIST_DIR + "/app",
+		filename: "bundle.js",
+		publicPath: "/app/"
+	},
+	module: {
+		loaders: [
+			{
+				test: /\.js?/,
+				include: SRC_DIR,
+				loader: "babel-loader",
+				query: {
+					presets: ["react", "es2015", "stage-2"],
+					plugins: ["transform-class-properties"]
+				},
+			},
+			{
+        test: /\.css$/,
+        loader: 'style-loader!css-loader'
+      },
+      {
+        test: /\.(png|jpg|gif|svg|eot|ttf|woff|woff2)$/,
+        loader: 'url-loader',
+        options: {
+          limit: 10000
+        }
+      }
+		]
+	}
+};
+
+module.exports = config;
