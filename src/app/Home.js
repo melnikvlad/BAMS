a06d184a3e86c078b3352ea06461787bf9201e64
Vladislav <vlad.melnikov.post@gmail.com>
2017-11-29 00:49:43 +0300

    Added carousel and white block

--- src/app/Home.js
+++ src/app/Home.js
@@ -22,51 +22,63 @@ export class Home extends React.Component {
               </section>
 
               <section className=".container-fluid col-sm-12 section_3">
-                <div className="col-sm-8 col-md-8 white">
-                  <p className="text">Cras facilisis urna ornare ex volutpat, et
-                  convallis erat elementum. Ut aliquam, ipsum vitae
-                  gravida suscipit, metus dui bibendum est, eget rhoncus nibh
-                  metus nec massa. Maecenas hendrerit laoreet augue
-                  nec molestie. Cum sociis natoque penatibus et magnis
-                  dis parturient montes, nascetur ridiculus mus.</p>
-                </div>
 
-                <div className="col-sm-8 col-md-8 white">
-                  <p className="text">Cras facilisis urna ornare ex volutpat, et
-                  convallis erat elementum. Ut aliquam, ipsum vitae
-                  gravida suscipit, metus dui bibendum est, eget rhoncus nibh
-                  metus nec massa. Maecenas hendrerit laoreet augue
-                  nec molestie. Cum sociis natoque penatibus et magnis
-                  dis parturient montes, nascetur ridiculus mus.</p>
-                </div>
+              <div class="bs-example">
+                <div id="myCarousel" class="carousel slide" data-ride="carousel">
 
-                <div className="col-sm-8 col-md-8 white">
-                  <p className="text">Cras facilisis urna ornare ex volutpat, et
-                  convallis erat elementum. Ut aliquam, ipsum vitae
-                  gravida suscipit, metus dui bibendum est, eget rhoncus nibh
-                  metus nec massa. Maecenas hendrerit laoreet augue
-                  nec molestie. Cum sociis natoque penatibus et magnis
-                  dis parturient montes, nascetur ridiculus mus.</p>
-                </div>
+                    <ol class="carousel-indicators">
+                        <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+                        <li data-target="#myCarousel" data-slide-to="1"></li>
+                        <li data-target="#myCarousel" data-slide-to="2"></li>
+                    </ol>
 
-                <div className="col-sm-8 col-md-8 white">
-                  <p className="text">Cras facilisis urna ornare ex volutpat, et
-                  convallis erat elementum. Ut aliquam, ipsum vitae
-                  gravida suscipit, metus dui bibendum est, eget rhoncus nibh
-                  metus nec massa. Maecenas hendrerit laoreet augue
-                  nec molestie. Cum sociis natoque penatibus et magnis
-                  dis parturient montes, nascetur ridiculus mus.</p>
-                </div>
+                    <div class="carousel-inner" role="listbox">
+                        <div class="item active">
+                            <div className="col-xs-offset-3 col-sm-8 col-md-6 white">
+                              <p className="text_title">Title 1</p>
+                              <p className="text">Cras facilisis urna ornare ex volutpat, et
+                              convallis erat elementum. Ut aliquam, ipsum vitae
+                              gravida suscipit, metus dui bibendum est, eget rhoncus nibh
+                              metus nec massa. Maecenas hendrerit laoreet augue
+                              nec molestie. Cum sociis natoque penatibus et magnis
+                              dis parturient montes, nascetur ridiculus mus.</p>
+                              <div className="farmer_devider"/>
+                            </div>
+                        </div>
+                        <div class="item">
+                          <div className="col-xs-offset-3 col-sm-8 col-md-6 white">
+                            <p className="text_title">Title 2</p>
+                            <p className="text">Cras facilisis urna ornare ex volutpat, et
+                            convallis erat elementum. Ut aliquam, ipsum vitae
+                            gravida suscipit, metus dui bibendum est, eget rhoncus nibh
+                            metus nec massa. Maecenas hendrerit laoreet augue
+                            nec molestie. Cum sociis natoque penatibus et magnis
+                            dis parturient montes, nascetur ridiculus mus.</p>
+                            <div className="farmer_devider"/>
+                          </div>
+                        </div>
+                        <div class="item">
+                          <div className="col-xs-offset-3 col-sm-8 col-md-6 white">
+                            <p className="text_title">Title 3</p>
+                            <p className="text">Cras facilisis urna ornare ex volutpat, et
+                            convallis erat elementum. Ut aliquam, ipsum vitae
+                            gravida suscipit, metus dui bibendum est, eget rhoncus nibh
+                            metus nec massa. Maecenas hendrerit laoreet augue
+                            nec molestie. Cum sociis natoque penatibus et magnis
+                            dis parturient montes, nascetur ridiculus mus.</p>
+                            <div className="farmer_devider"/>
+                          </div>
+                        </div>
+                    </div>
 
-                <div className="col-sm-8 col-md-8 white">
-                  <p className="text">Cras facilisis urna ornare ex volutpat, et
-                  convallis erat elementum. Ut aliquam, ipsum vitae
-                  gravida suscipit, metus dui bibendum est, eget rhoncus nibh
-                  metus nec massa. Maecenas hendrerit laoreet augue
-                  nec molestie. Cum sociis natoque penatibus et magnis
-                  dis parturient montes, nascetur ridiculus mus.</p>
+                    <a class="carousel-control left" href="#myCarousel" data-slide="prev">
+                        <span class="glyphicon glyphicon-chevron-left"></span>
+                    </a>
+                    <a class="carousel-control right" href="#myCarousel" data-slide="next">
+                        <span class="glyphicon glyphicon-chevron-right"></span>
+                    </a>
                 </div>
-
+              </div>
 
               </section>
 
